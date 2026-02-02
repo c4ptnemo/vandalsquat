8d5a4c957f542b824d31f3364078966c020e8578
c4ptnemo <c4ptnem0@proton.me>
2026-02-02 19:19:07 +0000

    Routes set and pages ported to their HTML templates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  allow_browser versions: :modern
+  helper_method :current_user, :logged_in?
 
-  # Changes to the importmap will invalidate the etag for HTML responses
-  stale_when_importmap_changes
+  def current_user
+    return @current_user if defined?(@current_user)
+    @current_user = User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    current_user.present?
+  end
+
+  def require_login
+    redirect_to login_path unless logged_in?
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
+  get "entries/index"
+  get "entries/new"
+  get "entries/details"
+  get "users/new"
+  get "users/show"
+  get "sessions/new"
   root "home#index"
+
+  get "/login",  to: "sessions#new"
+  get "/signup", to: "users#new"
+  get "/account", to: "users#show"
+
+  get "/entries", to: "entries#index"
+  get "/entries/new", to: "entries#new"            # map step
+  get "/entries/new/details", to: "entries#details" # details form step
+
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  post "/signup", to: "users#create"
 end
+
