48370c7cbf1f4d7bfe705af36cc7375cec3f28d2
c4ptnemo <c4ptnem0@proton.me>
2026-02-09 21:16:29 +0000

    download entries button

--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,10 @@ Rails.application.routes.draw do
   delete '/account', to: 'users#destroy', as: :delete_account
 
   # Entries
+  get '/entries/download', to: 'entries#download_all', as: :download_entries
   resources :entries, only: [:index, :new, :create, :edit, :update, :destroy]
   get "/entries/new/details", to: "entries#details"
-  get '/entries/download', to: 'entries#download_all', as: :download_entries
+
 
   # Two-Factor Authentication routes
 get '/account/two-factor', to: 'users#two_factor', as: 'two_factor'
